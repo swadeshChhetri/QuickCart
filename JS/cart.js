7a44674736f6f79b80e620afbc3cbc629a9435a1
Swadesh Chhetri <swadeshchhetri87@gmail.com>
2024-11-24 07:22:43 +0530

    added features

---
+++ JS/cart.js
@@ -0,0 +1,49 @@
+
+const CONVENIENCE_FEES = 99;
+let bagItemObjects;
+
+    let bagItemsStr = localStorage.getItem('bagItems');
+    bagItems = bagItemsStr ? JSON.parse(bagItemsStr) : [];
+
+    loadBagItemObjects();
+    displayBagItems();
+
+
+function loadBagItemObjects() {
+    bagItemObjects = bagItems.map(itemId => {
+        return items.find(item => item.id === itemId);
+    });
+}
+
+function displayBagItems() {
+    const containerElement = document.querySelector('.bag-items-container');
+    let innerHTML = '';
+
+    bagItemObjects.forEach(item => {
+        innerHTML += generateItemHTML(item);
+    });
+
+    containerElement.innerHTML = innerHTML;
+}
+
+function generateItemHTML(item) {
+    return `
+        <tr>
+            <td><img src="../${item.image}" alt="" /></td>
+            <td>${item.productName}</td>
+            <td>₹${item.current_price}</td>
+            <td><input type="number" value="1" /></td>
+            <td>
+                <div class="remove-from-cart" onclick="removeFromBag(${item.id})"><i class="far fa-times-circle"></i></div>
+            </td>
+        </tr>
+    `;
+}
+
+function removeFromBag(itemId) {
+    bagItems = bagItems.filter(bagItemId => bagItemId !== itemId);
+    localStorage.setItem('bagItems', JSON.stringify(bagItems));
+    loadBagItemObjects();
+    displayBagIcon(); 
+    displayBagItems();
+}
---
+++ README.md
@@ -0,0 +1,25 @@
+# QuickCart - E-Commerce Website
+
+A user-friendly e-commerce platform with product listings, shopping cart functionality, and checkout flow.
+
+## Demo
+- **Live Demo:** [https://lnkd.in/gyTGmRmJ](https://lnkd.in/gyTGmRmJ)
+- **Source Code:** [https://lnkd.in/gRzHyaZY](https://lnkd.in/gRzHyaZY)
+
+## Features
+- Product listings
+- Add to cart
+- Checkout flow
+- Responsive design
+
+## Tech Stack
+- HTML
+- CSS
+- JavaScript
+
+## Installation
+To run this project locally:
+```bash
+git clone https://github.com/yourusername/quickcart.git
+cd quickcart
+open index.html
